8f9e32f5c53bf0f5252f51763ad1ea41c3c08f11
ardhi.rofi <ardhi.rofi@elnusa.co.id>
2021-12-01 23:26:56 +0700

    add chat box, chat item

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,6 +7,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import menuImg from '../assets/img/icons/menu.png';
 import menuDotImg from '../assets/img/icons/menu-dot.png';
 import userImg from '../assets/img/icons/010-user.png';
+import attachImg from '../assets/img/icons/attachment.png';
+import submitImg from '../assets/img/icons/submit.png';
 
 import './Dashboard.css';
 
@@ -17,14 +19,14 @@ export default function Home() {
     return (
         <div>
             <div className="header-menu">
-                <div className="ds-pt-1 ds-mr-5">
-                    <img src={menuImg} height="30px"/>
+                <div className="ds-mr-5 icon-bg">
+                    <img alt="profile-menu" src={menuImg} height="28"/>
                 </div>
                 <div className="ds-mr-5 search-input">
                     <Input placeholder="Search"></Input>
                 </div>
-                <div className="ds-ml-1 ds-pt-1 room-pict">
-                    <img src={userImg} height="30px"/>
+                <div className="ds-ml-1 ds-mt-1 ds-mb-1 room-pict">
+                    <img alt="profile-pict" src={userImg} height="30"/>
                 </div>
                 <div className="ds-ml-5 ds-mt-1 room-title">
                     <div>room title</div>
@@ -33,17 +35,17 @@ export default function Home() {
                 <div className="ds-mr-5 room-search">
                     <Input className="transparent night-mode" placeholder="Search in chat"></Input>
                 </div>
-                <div className="ds-pt-4 ds-mr-5 ds-pr-4">
-                    <img src={menuDotImg} height="20px"/>
+                <div className="ds-mt-1 ds-mr-5 icon-bg">
+                    <img alt="rooms-menu" src={menuDotImg} height="22px"/>
                 </div>
             </div>
             <div className="dashboard-container">
                 <div className="left-menu">
-                    {[1,2,3].map(el => {
+                    {[2,2,3].map(el => {
                         return (
                             <div className="room-preview-box ds-flex">
-                                <div className="ds-ml-1 ds-mt-1 room-pict">
-                                    <img src={userImg} height="40px"/>
+                                <div className="ds-ml-1 ds-mt-1 ds-mb-1 room-pict">
+                                    <img alt="room-pict" src={userImg} height="40"/>
                                 </div>
                                 <div className="ds-ml-5 ds-mt-1 room-title">
                                     <div>room title {el}</div>
@@ -52,11 +54,42 @@ export default function Home() {
                                         <div className="txt-desc-meta ds-ml-2">room chatting preview</div>
                                     </div>
                                 </div>
+                                <div>
+                                    <div className="txt-desc-meta-sm">12/1/2021</div>
+                                    <div className="room-unread ds-mt-2 ds-ml-3">{el+10}</div>
+                                </div>
                             </div>
                         )
                     })}
                 </div>
-                <div className="chatbox">chatbox</div>
+                <div className="chat-container">
+                    <div className="chatbox ds-flex">
+                        <div className="ds-ml-1 ds-mt-1 ds-mb-1 room-pict">
+                            <img alt="friends-pict" src={userImg} height="40"/>
+                        </div>
+                        <div className="ds-ml-5 ds-mt-1">
+                            <div className="chat-username">friends name</div>
+                            <div className="ds-flex chat-content">
+                                <div className="txt-desc-sm">room chat text</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="txt-desc-meta-sm ds-mt-2">12/1/2021</div>
+                        </div>
+                    </div>
+
+                    <div className="texting-box ds-flex">
+                        <div className="ds-m-3 ds-mt-4 icon-bg">
+                            <img className="" alt="friends-pict" src={attachImg} height="26"/>
+                        </div>
+                        <div className="ds-mt-3">
+                            <Input className="texting-input" placeholder="Write your thought" />
+                        </div>
+                        <div className="ds-m-3 icon-bg">
+                            <img alt="friends-pict" src={submitImg} height="28"/>
+                        </div>
+                    </div>
+                </div>
             </div>
             {/* <div className="modal">modal</div> */}
         </div>
